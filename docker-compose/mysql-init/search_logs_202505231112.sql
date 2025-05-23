b26b4428a5277fa1e8ce8b8a9f70d68f80cb336f
chrysadima <126327782+chrysadima@users.noreply.github.com>
2025-05-23 11:15:39 +0200

    database

---
+++ docker-compose/mysql-init/search_logs_202505231112.sql
@@ -0,0 +1,4 @@
+INSERT INTO di_internet_technologies_project.search_logs (user_id,query,searched_at) VALUES
+	 (NULL,'workout music','2025-05-16 08:05:29'),
+	 (NULL,'relaxing songs','2025-05-16 08:05:29'),
+	 (NULL,'queen greatest hits','2025-05-16 08:05:29');
