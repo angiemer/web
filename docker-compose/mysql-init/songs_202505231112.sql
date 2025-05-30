04038eb895ae062261073fb266f2dae05bc0ee8f
angiemer <angiemer949@gmail.com>
2025-05-30 20:45:38 +0300

    deleting

--- docker-compose/mysql-init/songs_202505231112.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO di_internet_technologies_project.songs (title,youtube_id,created_at) VALUES
-	 ('Eye of the Tiger','btPJPFnesV4','2025-05-16 08:05:29'),
-	 ('Chillhop Essentials','x8D6gIaSjcA','2025-05-16 08:05:29'),
-	 ('Imagine','YkgkThdzX-8','2025-05-16 08:05:29');
--- docker-compose/mysql-init/users_202505231112.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO di_internet_technologies_project.users (first_name,last_name,username,password,email,created_at,avatar) VALUES
-	 ('chrysa','dima','chrysa','$2y$10$rFvr/dASyXpRKMT.dKqu4.jXk9RRJJwqZwupLEfJ5Ky.Y7YWVySGG','chrysadima@gmail.com','2025-05-20 08:52:46','https://via.placeholder.com/100.png'),
-	 ('test','ing','testing','$2y$10$qd6TJZg5YN.wXk5XW02PIOWQqBabK2g0XlX0.8N4574lyG/ZCUYW.','testing@test.com','2025-05-22 14:32:07','https://via.placeholder.com/100.png'),
-	 ('test2','ing2','testing2','$2y$10$Di9r3d3MCuZjxQhBWTmmouUNkDozg9NkCeeRVl2eZVhce7Fqd5Cjy','testing2@test.com','2025-05-22 21:53:44','https://via.placeholder.com/100.png');
